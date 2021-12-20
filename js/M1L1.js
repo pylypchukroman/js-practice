c6b7aa16963e779326570a86ac4fb1b632bbaa1d
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-20 19:21:47 +0200

    update

--- js/M1L1.js
+++ js/M1L1.js
@@ -1,63 +1,61 @@
+'use strict';
 // let a = 0;
-​
+
 // a = 1;
-​
+
 // const x = 5;
-​
-// const y = x / (a + 1) ;
-​
+// let y = x / (a + 1);​
 // console.log(y);
-​
 // console.log(10 % 2);
-​
-//console.log("Hello world!!!");
-​
+
+// console.log("Hello world!!!");
+
 /*-----------------*/
-​
+
 // const userId = 15; // number
 // const varFloat = 2.5; // number
 // const str = "hello world!!!"; // string
 // const varBool = false; // true или false
 // let selectedProduct = null;
 // let testUn; // undefined
-​
+
 // console.log(testUn);
 // console.log(typeof testUn);
-​
+
 /*------------------*/
-​
+
 // console.log('ok');
-​
+
 // alert("Hello World!!!");
-​
+
 // const isConfirm = confirm("Выбери свой вариант");
-​
+
 // console.log(typeof isConfirm);
 // console.log(isConfirm);
-​
-​
+
+
 // const userStr = prompt("Какой-то текст");
-​
+
 // console.log(typeof userStr);
 // console.log(userStr);
-​
+
 /*----------------*/
-​
+
 // const x = 10;
 // const y = 5;
-​
+
 // console.log(x + y); // 15
 // console.log(x - y); // 5
 // console.log(x * y); // 50
 // console.log(x ** y); // 100000
 // console.log(x / y); // 2
 // console.log(x % y); // 0
-​
+
 // let z = 4;
-​
+
 // z += y; // z = z + y;
-​
-​
+
+
 // console.log(x < y); // false
 // console.log(x > y); // true
 // console.log(x <= y); // false
@@ -66,84 +64,84 @@
 // console.log(x === y); // false
 // console.log(x != y); // true
 // console.log(x !== y); // true
-​
-​
+
+
 /*---------------*/
-​
+
 // const str = '15.5.15';
-​
+
 // console.log(Number(str)); // NaN
 // console.log(+str); // NaN
 // console.log(parseInt(str)); // 15
 // console.log(parseFloat(str)); //15.5
-​
+
 /*---------------*/
-​
+
 // const num = 10.3;
-​
+
 // console.log(Math.floor(num)); // 10
 // console.log(Math.ceil(num)); // 11
 // console.log(Math.round(num)); // 10
-​
+
 // console.log(Math.max(4, 5, 6, 7, 1, 2, 45, 87)); // 87
 // console.log(Math.min(4, 5, 6, 7, 1, 2, 45, 87)); // 1
-​
+
 // console.log(Math.pow(2, 10)); // 1024
-​
+
 // console.log(Math.random()); // от 0 до 1
-​
+
 // console.log(parseInt(Math.random() * (10 - 1) + 1)); // целое от 1 до 10 (не включительно)
-​
+
 /*----------------*/
-​
+
 // const name = "Bill";
-​
+
 // const hello = "hello";
-​
+
 // const productName = "Repair droid";
-​
+
 // console.log(name[0]);
 // console.log(name[1]);
 // console.log(name[2]);
 // console.log(name[3]);
-​
+
 // const x = 2;
 // console.log(hello + ' ' + name);
 // console.log(`${hello} ${name} ${10 ** x}`);
-​
+
 // const bigName = name.toUpperCase();
-​
+
 // console.log(name.length);
 // console.log(name.toLowerCase());
 // console.log(bigName);
-​
+
 // console.log(hello.indexOf('el'));
-​
+
 // console.log(name.includes('B'));
-​
+
 // console.log(name.replace('l', 'Z'));
 // console.log(name.replaceAll('l', 'z'));
-​
+
 // console.log(productName.slice(4,9));
-​
+
 // const startIndex = productName.indexOf('i');
 // const endIndex = productName.indexOf('o');
-​
+
 // console.log(productName.slice(startIndex, endIndex));
-​
+
 /*--------------*/
-​
+
 // const a = 5;
 // const b = 10;
-​
+
 // console.log(true && a < b);
 // console.log(true || a > b);
-​
+
 // const t = 0 || 5;
-​
+
 // console.log(t);
-​
-​
+
+
 // const test1 = 45 < 10 && (78 > 10 || 5 > 1);
 // const test2 = !((78 < 5 && 5 > 45) || 6 < 3 || 7 > 1) && true;
 // const test3 = (!true || false) && true
--- js/M6L1.js
+++ js/M6L1.js
@@ -1,65 +1,64 @@
 // const body = document.body;
-​
+
 // console.log(body.childNodes);
 // console.log(body.children[1].firstElementChild.nextElementSibling.parentElement);
-​
+
 // console.log(body.querySelector('#test-i'));
 // console.log(body.querySelectorAll('.test-class'));
 // console.log(body.querySelector('p [test-attr="15"]'));
-​
+
 // console.log(document.getElementById('test-i'));
 // console.log(document.getElementsByClassName('test-class'));
 // console.log(document.getElementsByTagName('p'));
-​
-​
+
 // console.log(document.querySelector('p').textContent);
 // console.log(document.querySelector('p').innerHTML);
 // console.log(document.querySelector('p').outerHTML);
-​
+
 // const span = document.querySelector('span.test-15');
-​
+
 // console.log([...span.classList]);
-​
+
 // span.classList.add('hello');
 // span.classList.remove('test-15');
 // span.classList.toggle('test-15');
 // span.classList.replace('test-class', 'new-test-class');
-​
+
 // console.log(span.classList.contains('test-class'));
 // console.log(span.classList.contains('hello'));
-​
+
 // console.log([...span.classList]);
-​
+
 // const h1 = document.querySelector('h1');
-​
+
 // h1.style.fontFamily = "arial";
 // h1.style.fontSize = "40px";
 // h1.style.backgroundColor = "tomato";
 // h1.style.padding = "10px";
 // h1.style.textAlign = "center";
-​
+
 // const nowPadding = parseInt(h1.style.padding);
-​
+
 // h1.style.padding = `${nowPadding+5}px`;
-​
+
 // const b = document.querySelector('b.exapmle-class');
-​
+
 // console.log(b.hasAttribute('test-attr'));
 // console.log(b.getAttribute('test-attr'));
 // b.setAttribute('new-test-attr', '962');
 // b.removeAttribute('test-attr');
-​
+
 // console.log(b.attributes);
-​
+
 // console.log(b);
-​
+
 // const a = document.querySelector('a');
-​
+
 // console.log(a.href);
 // console.log(a.getAttribute('href'));
 // a.setAttribute('href', 'link-test');
 // console.log(a.href);
-​
+
 // const divNode = document.createElement('div');
 // ​
 // divNode.textContent = 'new Div element';
@@ -73,17 +72,17 @@
 // divNode.style.height = `100px`;
 // ​
 // divNode.dataset.action = 'test-15';
-​
+
 // console.log(divNode.dataset.action);
-​
+
 // divNode.setAttribute('data-new-action', 'test-96');
-​
+
 // console.log(divNode.dataset.newAction);
-​
+
 // const divNodeClone = divNode.cloneNode(true);
-​
+
 // console.log(divNode);
-​
+
 // document.body.append(divNode); // Elem
 // document.body.prepend(divNode); // Elem
 // document.querySelector('h1').after(divNode); // Elem
@@ -92,18 +91,18 @@
 // divNode.remove();
 // ​
 // const pNode = document.querySelector('p');
-​
-pNode.innerHTML = divNode.outerHTML; // Text
+
+// pNode.innerHTML = divNode.outerHTML; // Text
 // pNode.innerHTML = pNode.innerHTML + divNode.outerHTML;
 // pNode.innerHTML += divNode.outerHTML;
 // pNode.innerHTML += divNode.outerHTML;
 // pNode.innerHTML += divNode.outerHTML;
 // pNode.outerHTML = '<span>test</span>';
-​
-const markup = '<span>55</span>';
-​
-pNode.textContent = 'TEST TEXT' // Text
-​
-pNode.insertAdjacentHTML("beforeend", markup); // Text
-pNode.insertAdjacentText("beforeend", markup); // Text
-pNode.insertAdjacentElement("beforeend", divNode); // Elem
+
+// const markup = '<span>55</span>';
+// ​
+// pNode.textContent = 'TEST TEXT' // Text
+// ​
+// pNode.insertAdjacentHTML("beforeend", markup); // Text
+// pNode.insertAdjacentText("beforeend", markup); // Text
+// pNode.insertAdjacentElement("beforeend", divNode); // Elem
