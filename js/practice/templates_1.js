84b3cbd47ae548d1a9c89bc5bede897a234b1c99
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-20 22:11:06 +0200

    Події на кнопках

--- js/practice/templates_1.js
+++ js/practice/templates_1.js
@@ -1,3 +1,22 @@
 'use strict';
 
 console.log("OK_1");
+
+
+const redButton = document.querySelector('.red');
+const blueButton = document.querySelector('.blue');
+const greenButton = document.querySelector('.green');
+
+const colors = function (color) {
+    document.body.style.backgroundColor = (color);
+}
+
+redButton.addEventListener('click', () => {
+    colors('red')
+});
+blueButton.addEventListener('click', () => {
+    colors('blue')
+});
+greenButton.addEventListener('click', () => {
+    colors('green')
+});
