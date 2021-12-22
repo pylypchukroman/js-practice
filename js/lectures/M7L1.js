feea683cf0902f7fcb1047ad7894de909b2ed0e2
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-22 21:11:54 +0200

    update

--- js/lectures/M7L1.js
+++ js/lectures/M7L1.js
@@ -1,3 +1,44 @@
 'use strict';
 
 console.log("OK");
+
+// const handler1 = e => {
+//     e.stopImmediatePropagation();
+//     //e.stopPropagation();
+//     console.log(e.currentTarget, e.target);
+// }
+// const handler2 = e => {
+//     console.log(e.currentTarget, e.target);
+// }
+
+
+// document.querySelector('#div-3').addEventListener('click', handler1);
+// document.querySelector('#div-3').addEventListener('click', handler2);
+
+
+// document.querySelector('#div-2').addEventListener('click', handler1);
+// document.querySelector('#div-1').addEventListener('click', handler1);
+
+/*-----------------*/
+
+// document.querySelector('#root').addEventListener('click', e => {
+//     if (e.target.closest('button').tagName !== 'BUTTON' || !e.target.dataset.color) return false;
+
+//     document.body.style.backgroundColor = e.target.dataset.color;
+// });
+
+const refs = {
+    panel: document.querySelector('#panel'),
+    num: document.querySelector('#num'),
+    calc: document.querySelector('#calc'),
+    input: document.querySelector('#calc input'),
+}
+
+refs.calc.addEventListener('click', e => {
+    if (!e.target.closest('button')) return false;
+    const value = e.target.textContent;
+
+    if (value === '=') refs.input.value = eval(refs.input.value);
+    else if (value === 'clear') refs.input.value = '';
+    else refs.input.value += value;
+})
--- js/practice/templates_2.js
+++ js/practice/templates_2.js
@@ -2,3 +2,52 @@
 
 console.log("OK_2");
 
+const refs = {
+  ball: document.querySelector("#ball"),
+  root: document.querySelector("#root"),
+};
+
+const moveBall = _.throttle((step = 1, dir = "x") => {
+  const nowPos = {
+    x: parseInt(refs.ball.style.left),
+    y: parseInt(refs.ball.style.top),
+  };
+  const maxPos = {
+    topR: {
+      x: refs.root.clientWidth - refs.ball.offsetWidth,
+      y: 0,
+    },
+    bottomR: {
+      x: refs.root.clientWidth - refs.ball.offsetWidth,
+      y: refs.root.clientHeight - refs.ball.offsetHeight,
+    },
+    bottomL: {
+      x: 0,
+      y: refs.root.clientHeight - refs.ball.offsetHeight,
+    },
+    topL: {
+      x: 0,
+      y: 0,
+    },
+  };
+
+  nowPos[dir] += step;
+
+  refs.ball.style.left = nowPos.x + "px";
+  refs.ball.style.top = nowPos.y + "px";
+
+  if (maxPos.topR.x === nowPos.x && dir === "x") {
+    dir = "y";
+  } else if (maxPos.bottomR.y === nowPos.y && dir === "y") {
+    dir = "x";
+    step *= -1;
+  } else if (maxPos.bottomL.x === nowPos.x && dir === "x") {
+    dir = "y";
+  } else if (maxPos.topL.y === nowPos.y && dir === "y") {
+    dir = "x";
+    step *= -1;
+  }
+    // moveBall(step, dir);
+}, 10);
+
+moveBall();
