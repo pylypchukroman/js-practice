20122c6a6c783eda55edb12b29b4d68519272db1
pylypchukroman <romanpylypchuk@gmail.com>
2021-12-22 18:12:16 +0200

    12

--- js/lectures/M6L2.js
+++ js/lectures/M6L2.js
@@ -1,3 +1,71 @@
 'use strict';
 
 console.log("OK");
+
+// const refs = {
+//     button: document.querySelector('#button-1'),
+
+//     div: document.querySelector('#content'),
+//     textarea: document.querySelector('#text-1'),
+
+//     input: document.querySelector('#input-id'),
+//     form: document.querySelector('form'),
+//     checkbox: document.querySelector('#checkbox-id'),
+//     selector: document.querySelector('#selector')
+// }
+
+// const buttonClick = event => {
+//     console.log(event.currentTarget);
+// };
+
+// refs.button.addEventListener("click", buttonClick)
+
+// refs.button.addEventListener("keyup", e => {
+//     console.log(e.code);
+//     if (e.code === 'Space')
+//         refs.button.removeEventListener("click", buttonClick);
+// })
+
+// refs.input.addEventListener('change', e => {
+//     console.log(e);
+// });
+
+// refs.input.addEventListener('input', e => {
+//     console.log(e);
+// });
+
+// refs.checkbox.addEventListener('change', console.log)
+// refs.selector.addEventListener('change', console.log)
+
+// refs.form.addEventListener("submit", e => {
+//     e.preventDefault();
+//     console.log(e);
+// });
+
+// refs.input.addEventListener('focus', e => {
+//     console.log(e);
+// });
+
+// refs.input.addEventListener('blur', e => {
+//     console.log(e);
+// });
+
+
+// const handler = e => {
+//    // if (e.code === 'Enter' && e.ctrlKey)
+//         console.log(e.type);
+// }
+
+// refs.div.addEventListener('click', handler);
+// refs.div.addEventListener('mouseup', handler);
+// refs.div.addEventListener('mousedown', handler);
+
+// refs.div.addEventListener('mouseover', handler);
+// refs.div.addEventListener('mousemove', handler);
+// refs.div.addEventListener('mouseout', handler);
+
+// refs.textarea.addEventListener('keydown', handler);
+// refs.textarea.addEventListener('keypress', handler);
+// refs.textarea.addEventListener('keyup', handler);
+
+/*-----------------*/
